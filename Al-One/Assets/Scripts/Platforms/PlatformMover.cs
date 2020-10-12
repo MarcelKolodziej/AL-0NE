26a85712f261790e4e6b9523cdbf1a762d3c672f
Elliot <elliotgriffiss@gmail.com>
2020-10-12 16:35:45 +0100

    Short Level and Moving Platforms
    -Created a test bed scene to mess around with the tile system
    - Fixed grass form under dirt
    - Created a script to move platforms

---
+++ Al-One/Assets/Scripts/Platforms/PlatformMover.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMover : MonoBehaviour
+{
+    public Transform MovingTile;
+    public Transform[] Locations;
+
+    public byte LocationIndex = 0;
+    public float TileSpeed =1f;
+    private float m_Step;
+    private Vector3 m_Direction;
+
+    private void Update()
+    {
+        if (MovingTile.position != Locations[LocationIndex].position)
+        {
+            m_Step = TileSpeed * Time.deltaTime;
+            MovingTile.position = Vector3.MoveTowards(MovingTile.position, Locations[LocationIndex].position, m_Step);
+        }
+        else
+        {
+            LocationIndex++;
+
+            if (LocationIndex > Locations.Length-1)
+            {
+                LocationIndex = 0;
+            }
+        }
+    }
+}
