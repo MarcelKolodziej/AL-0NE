a58be595cff6e345f445f21f2190395f6515313e
Elliot <elliotgriffiss@gmail.com>
2020-10-12 20:36:26 +0100

    Further Improvements
    - Added an animation curve to the platform mover for a more customizable interface
    - Added more to the level
    - Leanrt how to create random brushes and filled in the levels background

--- Al-One/Assets/Scripts/Platforms/PlatformMover.cs
+++ Al-One/Assets/Scripts/Platforms/PlatformMover.cs
@@ -9,25 +9,69 @@ public class PlatformMover : MonoBehaviour
     [SerializeField] private Transform[] Locations;
 
     [Header("Settings")]
-    [SerializeField] private byte LocationIndex = 0;
-    [SerializeField] private float TileSpeed =1f;
+    [SerializeField] private AnimationCurve AnimationCurve = AnimationCurve.Linear(0,0,1,1);
+    [SerializeField] private int StartingLocation = 0;
+    [SerializeField] private float TileMoveSpeed = 1f;
 
+    [Header("Public Fields")]
+    public bool IsActive = true;
+    public bool PlayOneshot = false; // Added this so we can sequence a bunch of platforms at the same time  and ensure they stay in sync
+
+    private float startTime;
     private float step;
+    private int currentLocationIndex = 0;
+    private int targetLocationIndex = 0;
+    private float journeyLenght;
+
+    private void Start()
+    {
+        MovingTile.position = Locations[StartingLocation].position;
+        currentLocationIndex = StartingLocation -1;  // this is stupid, but it prevents duplicate code
+        targetLocationIndex = StartingLocation;
+        UpdatePlatformTargets();
+    }
+
+    private void UpdatePlatformTargets()
+    {
+        currentLocationIndex++;
+
+        if (currentLocationIndex > Locations.Length - 1)
+        {
+            currentLocationIndex = 0;
+        }
+
+        targetLocationIndex++;
+
+        if (targetLocationIndex > Locations.Length - 1)
+        {
+            targetLocationIndex = 0;
+        }
+
+        startTime = Time.time;
+        journeyLenght = Vector3.Distance(Locations[currentLocationIndex].position, Locations[targetLocationIndex].position);
+    }
 
     private void Update()
     {
-        if (MovingTile.position != Locations[LocationIndex].position)
+        if (IsActive)
         {
-            step = TileSpeed * Time.deltaTime;
-            MovingTile.position = Vector3.MoveTowards(MovingTile.position, Locations[LocationIndex].position, step);
-        }
-        else
-        {
-            LocationIndex++;
+            float distanceCovered = (Time.time - startTime) * TileMoveSpeed;
 
-            if (LocationIndex > Locations.Length-1)
+            if (distanceCovered < journeyLenght)
             {
-                LocationIndex = 0;
+                float fractionOfJourney = distanceCovered / journeyLenght;
+
+                MovingTile.position = Vector3.Lerp(Locations[currentLocationIndex].position, Locations[targetLocationIndex].position, AnimationCurve.Evaluate(fractionOfJourney));
+            }
+            else
+            {
+                UpdatePlatformTargets();
+
+                if (PlayOneshot)
+                {
+                    PlayOneshot = false;
+                    IsActive = false;
+                }
             }
         }
     }
