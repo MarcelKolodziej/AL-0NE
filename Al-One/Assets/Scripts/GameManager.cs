8fd3b2e7ca4f12288bd6b587cd92f53c6f783362
Elliot <elliotgriffiss@gmail.com>
2020-10-13 15:58:48 +0100

    GameManager and Fixes
    - Game Sequencer is now fixed and it's possible to get to the end of the level
    - Added a GameManager to handle loading the player and camera into game and assign the correct values to them. It can also manage the spawn point and be used to trigger the players death.
    - GameManager can be improved to keep track of health, score, ect:
    - Added the ability to respawn the player on death
    - Added a simgple script to trigger death when a collider is touched
    - Re-added the player tag so the break on touch stuff stil works
    - Added a tag to the camera prefab so the GameManager can access it efficently.

---
+++ Al-One/Assets/Scripts/GameManager.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Cinemachine;
+
+/// <summary>
+/// Game Manager Singleton, use this for keeping track of player progress, loading levels, health, respawning the player
+/// </summary>
+public class GameManager : MonoBehaviour
+{
+    private static GameManager gameManager = null;
+
+    public static GameManager Instance
+    {
+        get
+        {
+            return gameManager;
+        }
+    }
+
+    [SerializeField] private GameObject PlayerPrefab;
+    [SerializeField] private GameObject CameraPrefab;
+
+    private PlayerControls playerControls;
+    private Transform spawnPoint;
+    private CinemachineVirtualCamera cmvCamera;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+
+        gameManager = this;
+        DontDestroyOnLoad(gameObject);
+        SceneManager.sceneLoaded += OnLevelFinishedLoading;
+    }
+    private void OnDestroy()
+    {
+        SceneManager.sceneLoaded -= OnLevelFinishedLoading;
+    }
+
+    private void OnLevelFinishedLoading(Scene scene, LoadSceneMode mode)
+    {
+        GameObject gameObject;
+
+        // Populate references
+        SetSpawnPoint(GameObject.FindGameObjectWithTag("Respawn"));
+
+        gameObject = GameObject.Instantiate(PlayerPrefab);
+        playerControls = gameObject.GetComponent<PlayerControls>();
+
+        gameObject = GameObject.Instantiate(CameraPrefab);
+        cmvCamera = gameObject.GetComponentInChildren<CinemachineVirtualCamera>();
+        cmvCamera.Follow = playerControls.transform;
+
+        // Setup Game Scene
+        RespawnPlayer();
+    }
+
+    public void SetSpawnPoint(GameObject SpawnPoint)
+    {
+        spawnPoint = SpawnPoint.transform;
+    }
+    public void RespawnPlayer()
+    {
+        playerControls.transform.position = spawnPoint.transform.position;
+        cmvCamera.transform.position = spawnPoint.transform.position;
+    }
+    public void DamangePlayer()
+    {
+        TriggerPlayerDeath();
+    }
+
+    public void TriggerPlayerDeath()
+    {
+        RespawnPlayer();
+    }
+}
