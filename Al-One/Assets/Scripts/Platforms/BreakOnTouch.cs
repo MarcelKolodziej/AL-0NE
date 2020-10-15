aa6820cb4bc44a20ae2cdaafad6b1e16ed790e5b
Elliot <elliotgriffiss@gmail.com>
2020-10-15 15:48:49 +0100

    Updated the break on touch so it looks nicer

--- Al-One/Assets/Scripts/Platforms/BreakOnTouch.cs
+++ Al-One/Assets/Scripts/Platforms/BreakOnTouch.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class BreakOnTouch : MonoBehaviour
 {
+    [Header("Objects To Assign")]
+    [SerializeField] private Rigidbody2D ObjectRigidbody;
+
     [Header("Settings")]
-    [SerializeField] private float breakDelay = 1f;
+    [SerializeField] private float DropDelay = 1f;
+    [SerializeField] private float BreakDelay = 1f;
 
     /// <summary>
     /// Detect Collion with player character and start break block timer
@@ -24,7 +28,9 @@ public class BreakOnTouch : MonoBehaviour
     /// </summary>
     private IEnumerator WaitForBreak()
     {
-        yield return new WaitForSeconds(breakDelay);
+        yield return new WaitForSeconds(DropDelay);
+        ObjectRigidbody.bodyType = RigidbodyType2D.Dynamic;
+        yield return new WaitForSeconds(BreakDelay);
         gameObject.SetActive(false);
     }
 }
