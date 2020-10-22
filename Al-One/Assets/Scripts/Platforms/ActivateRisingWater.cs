f10ea579b20fcb590ef9dd1f8d75c113979999bb
Elliot <elliotgriffiss@gmail.com>
2020-10-22 08:04:22 +0100

    Small Fixes
    - Visual Change in the hub to fix assets not appearing as expected
    - Door now closes behind the player when the water starts rising to prevent them from backtracking and reseting the water

--- Al-One/Assets/Scripts/Platforms/ActivateRisingWater.cs
+++ Al-One/Assets/Scripts/Platforms/ActivateRisingWater.cs
@@ -6,12 +6,14 @@ public class ActivateRisingWater : MonoBehaviour
 {
     [Header("Objects To Assign")]
     [SerializeField] private RisingWater RisingWater;
+    [SerializeField] private PlatformMover Door;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Player")
         {
             RisingWater.ActivateRisingWater();
+            Door.ResetToDefaultPosition(true);
         }
     }
 }
