86792abf0438837daaebf627b608b1c7963d5683
Elliot <elliotgriffiss@gmail.com>
2020-10-24 11:08:31 +0100

    Home World Changes
    - Added a little note to tell the player they can activate the jetpack using the Konami code at the end of the game

--- Al-One/Assets/Scripts/UI/EndingSlideCloseButton.cs
+++ Al-One/Assets/Scripts/UI/EndingSlideCloseButton.cs
@@ -4,8 +4,23 @@ using UnityEngine;
 
 public class EndingSlideCloseButton : MonoBehaviour
 {
+    [SerializeField] private GameObject EndingMessage;
+    [SerializeField] private GameObject KonamiHintMessage;
+
+
+    private void OnEnable()
+    {
+        EndingMessage.SetActive(true);
+        KonamiHintMessage.SetActive(false);
+    }
     public void OnCloseButtonPressed()
     {
         gameObject.SetActive(false);
+    }
+
+    public void OnNextButtonPressed()
+    {
+        EndingMessage.SetActive(false);
+        KonamiHintMessage.SetActive(true);
     }
 }
