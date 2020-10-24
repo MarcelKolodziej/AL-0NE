c54aed9f504297ba6b2010acf67be6c6d5397e76
Elliot <elliotgriffiss@gmail.com>
2020-10-24 13:02:56 +0100

    Menu Fixes Again
    Hopefully this is the last time

--- Al-One/Assets/Scripts/UI/MenuTrigger.cs
+++ Al-One/Assets/Scripts/UI/MenuTrigger.cs
@@ -5,12 +5,24 @@ using UnityEngine;
 public class MenuTrigger : MonoBehaviour
 {
     [SerializeField] private GameObject Menu;
+    [SerializeField] private bool IsMainMenu = false;
     private bool IsNearMenu = false;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         IsNearMenu = true;
-        Menu.SetActive(true);
+
+        if (IsMainMenu)
+        {
+            if (GameManager.Instance.MainMenuClosed == false)
+            {
+                Menu.SetActive(true);
+            }
+        }
+        else
+        {
+            Menu.SetActive(true);
+        }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
